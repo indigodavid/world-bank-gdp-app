2000eb6fe6c65b3a653728237cc8c3508cbe6f1f
David Vera Castillo <indigodavid@gmail.com>
2022-07-27 14:32:36 -0500

    Add functionality to Details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { fetchCountryGDP } from '../redux/countries/countries';
 
 const Details = () => {
-  const { country } = useParams();
+  const { countryCode } = useParams();
+  const dispatch = useDispatch();
+  const country = useSelector((state) => state.countries)
+    .find((country) => country.id === countryCode);
+  useState(() => {
+    if (country.gdp.length < 2) {
+      dispatch(fetchCountryGDP(countryCode));
+    }
+  }, []);
+  const { gdp } = country;
+
   return (
-    <div>
-      {country}
-    </div>
+    <ul>
+      {gdp.map((indicator) => (
+        <li key={`${countryCode}-${indicator.date}`}>
+          <span>
+            Date:
+            {indicator.date}
+          </span>
+          <span>
+            GDP:
+            {indicator.value ? indicator.value : 'Data not available'}
+          </span>
+        </li>
+      ))}
+    </ul>
   );
 };
 
