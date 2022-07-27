18403b02a9f82ef749b7b429cdfeeb1ba73e209b
David Vera Castillo <indigodavid@gmail.com>
2022-07-26 21:06:26 -0500

    Add countries route with slug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './styles/App.scss';
 import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/countries/:country" element={<Details />} />
     </Routes>
   );
 }
