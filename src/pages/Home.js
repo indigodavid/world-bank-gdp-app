22c2899ff82f7fd866d4299b2db5a73f68c04b00
David Vera Castillo <indigodavid@gmail.com>
2022-07-27 11:46:12 -0500

    Remove dispatch fetchGDP from Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,10 @@
-import React, { useEffect, useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchGDP } from '../redux/regions/regions';
+import Indicator from '../components/Indicator';
 
 const Home = () => {
   const regions = useSelector((state) => state.regions);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (regions.length !== 0) {
-      regions.forEach((region) => {
-        if (region.gdp.length < 2) {
-          dispatch(fetchGDP(region.code));
-        }
-      });
-    }
-  }, []);
-
-  useMemo(() => {
-    regions.forEach((region) => {
-      if (region.gdp.length < 2) {
-        dispatch(fetchGDP(region.code));
-      }
-    });
-  }, [{ regions }]);
 
   return (
     <ul>
@@ -32,7 +14,7 @@ const Home = () => {
           {' '}
           <span>{region.code}</span>
           <span>GDP:</span>
-          <span>{region.gdp[0] !== 0 ? region.gdp[0].value : 'No Data'}</span>
+          <Indicator regionCode={region.code} />
           <Link to={`/countries/${region.code}`}>{region.code}</Link>
         </li>
       ))}
