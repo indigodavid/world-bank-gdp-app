d95bacecce123c7e682f387c8df09a7e224dc538
David Vera Castillo <indigodavid@gmail.com>
2022-07-27 11:40:53 -0500

    Fix get regions function to obtain the ones that classify countries

--- src/utils/getRegionsFromAPI.js
+++ src/utils/getRegionsFromAPI.js
@@ -3,7 +3,7 @@ import { REGIONS_URL } from './API_ROUTES';
 const getRegionsFromAPI = async () => {
   const response = await fetch(REGIONS_URL);
   const unfilteredRegions = await response.json();
-  const regions = unfilteredRegions[1];
+  const regions = unfilteredRegions[1].filter((region) => region.id);
   return regions;
 };
 
