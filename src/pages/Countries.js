df12347ce2d7d09153cd0473b3bb815632550f6a
David Vera Castillo <indigodavid@gmail.com>
2022-07-27 11:49:00 -0500

    Add Countries page

---
+++ src/pages/Countries.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+// import Indicator from '../components/Indicator';
+
+const Countries = () => {
+  const { regionCode } = useParams();
+  const countries = useSelector((state) => state.countries)
+    .filter((country) => regionCode === country.region.id);
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id} className="country">
+          <span>{country.name}</span>
+          {' '}
+          <span>{country.id}</span>
+          <span>GDP:</span>
+          <Link to={`/deatils/${country.id}`}>{country.id}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Countries;
