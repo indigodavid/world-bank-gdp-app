2546977983ec1e6a94b4a1c3cbcd8ef2b2a1f50a
David Vera Castillo <indigodavid@gmail.com>
2022-07-26 21:07:42 -0500

    Add methods to generate URLs for the API

--- src/utils/API_ROUTES.js
+++ src/utils/API_ROUTES.js
@@ -1,5 +1,24 @@
 const JSON_FORMAT = '?format=json';
 const BASE_URL = 'http://api.worldbank.org/v2/';
 const REGIONS_URL = `${BASE_URL}region/${JSON_FORMAT}`;
+const GDP_INDICATOR = 'NY.GDP.MKTP.CD';
 
-export { BASE_URL, REGIONS_URL };
+const generateCountryURL = (countryCode) => (
+  `${BASE_URL}/country/${countryCode}/${JSON_FORMAT}`
+);
+
+const generateCountryGdpURL = (countryCode) => (
+  `${BASE_URL}/country/${countryCode}/indicator/${GDP_INDICATOR}${JSON_FORMAT}`
+);
+
+const generateCountriesURL = (regionCode) => (
+  `${BASE_URL}/country/${regionCode}/${JSON_FORMAT}`
+);
+
+export {
+  BASE_URL,
+  REGIONS_URL,
+  generateCountryGdpURL,
+  generateCountryURL,
+  generateCountriesURL,
+};
