870a9575ff213594be4465aee056e3a08fcbcfcd
David Vera Castillo <indigodavid@gmail.com>
2022-07-27 11:49:37 -0500

    Add routes for Countries

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './styles/App.scss';
 import Home from './pages/Home';
 import Details from './pages/Details';
+import Countries from './pages/Countries';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/countries/:country" element={<Details />} />
+      <Route path="/countries/:regionCode" element={<Countries />} />
+      <Route path="/details/:country" element={<Details />} />
     </Routes>
   );
 }
