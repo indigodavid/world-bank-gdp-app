b4998d6c599ecf456f5e030f7f0a39f242ca9752
David Vera Castillo <indigodavid@gmail.com>
2022-07-29 02:03:37 -0500

    Use HTTPS to secure deployment

--- src/utils/API_ROUTES.js
+++ src/utils/API_ROUTES.js
@@ -1,5 +1,5 @@
 const JSON_FORMAT = '?format=json';
-const BASE_URL = 'http://api.worldbank.org/v2/';
+const BASE_URL = 'https://api.worldbank.org/v2/';
 const REGIONS_URL = `${BASE_URL}region/${JSON_FORMAT}`;
 const GDP_INDICATOR = 'NY.GDP.MKTP.CD';
 
