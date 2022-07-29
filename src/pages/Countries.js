056ce6407401fb62e417961f041ad7b26f366676
David Vera Castillo <indigodavid@gmail.com>
2022-07-28 21:43:29 -0500

    Refactor Countries for styling

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import Indicator from '../components/Indicator';
+import Loading from '../components/Loading';
 
 const Countries = () => {
   const { regionCode } = useParams();
   const countries = useSelector((state) => state.countries)
     .filter((country) => regionCode === country.region.id);
   return (
-    <ul>
+    <ul className="countries">
       {countries.map((country) => (
         <li key={country.id} className="country">
-          <span>{country.name}</span>
-          {' '}
-          <span>{country.id}</span>
-          <span>GDP:</span>
-          <Indicator countryCode={country.id} />
-          <Link to={`/details/${country.id}`}>{country.id}</Link>
+          <Link to={`/details/${country.id}`}>
+            <div className="countryInfo">
+              <span className="countryTitle">{country.name}</span>
+              <span>
+                {<Indicator countryCode={country.id} /> || <Loading />}
+              </span>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
