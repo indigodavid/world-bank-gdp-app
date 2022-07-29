972ee7656afa3f4958d5672299cfc8c26a24b168
David Vera Castillo <indigodavid@gmail.com>
2022-07-28 23:43:31 -0500

    Create Menu for Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,24 +8,39 @@ import MapChart from '../components/MapChart';
 const Home = () => {
   const regions = useSelector((state) => state.regions);
 
+  const hideMenu = () => {
+    document.getElementById('menu').style.display = 'none';
+  };
+
   return (
-    <ul className="regions">
-      {regions.map((region) => (
-        <li key={region.code} className="region">
-          <Link to={`/countries/${region.code}`}>
-            <div>
-              {<MapChart region={region.code} /> || <Loading />}
-            </div>
-            <div className="regionInfo">
-              <span className="regionTitle">{region.name}</span>
-              <span>
-                {<Indicator regionCode={region.code} /> || <Loading />}
-              </span>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div className="wrapper">
+      <ul className="regions">
+        {regions.map((region) => (
+          <li key={region.code} className="region">
+            <Link to={`/countries/${region.code}`}>
+              <div className="map">
+                {<MapChart region={region.code} /> || <Loading />}
+              </div>
+              <div className="regionInfo">
+                <span className="regionTitle">{region.name}</span>
+                <span>
+                  {<Indicator regionCode={region.code} /> || <Loading />}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <ul id="menu">
+        {regions.map((region) => (
+          <li key={`${region.code}-menu`} className="region">
+            <Link to={`/countries/${region.code}`} onClick={hideMenu}>
+              {region.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
