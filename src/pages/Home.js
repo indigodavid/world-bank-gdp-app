e319c78863cf6f7fbdb95c89baf64f0a90910c63
David Vera Castillo <indigodavid@gmail.com>
2022-07-28 21:42:11 -0500

    Refactor Home for styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Indicator from '../components/Indicator';
+import Loading from '../components/Loading';
+import MapChart from '../components/MapChart';
 
 const Home = () => {
   const regions = useSelector((state) => state.regions);
 
   return (
-    <ul>
+    <ul className="regions">
       {regions.map((region) => (
         <li key={region.code} className="region">
-          <span>{region.name}</span>
-          {' '}
-          <span>{region.code}</span>
-          <span>GDP:</span>
-          <Indicator regionCode={region.code} />
-          <Link to={`/countries/${region.code}`}>{region.code}</Link>
+          <Link to={`/countries/${region.code}`}>
+            <div>
+              {<MapChart region={region.code} /> || <Loading />}
+            </div>
+            <div className="regionInfo">
+              <span className="regionTitle">{region.name}</span>
+              <span>
+                {<Indicator regionCode={region.code} /> || <Loading />}
+              </span>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
