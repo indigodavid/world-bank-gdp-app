f1605984c6212c5e0ee5888eaf2f1b79f9909168
David Vera Castillo <indigodavid@gmail.com>
2022-12-08 15:29:33 -0500

    ✅ Include Search Reducer on Redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import countriesReducer, { fetchCountries } from './countries/countries';
 import regionsReducer, { fetchRegions } from './regions/regions';
+import searchReducer from './search/search';
 
 const rootReducer = combineReducers({
   regions: regionsReducer,
   countries: countriesReducer,
+  search: searchReducer,
 });
 
 const store = configureStore({
