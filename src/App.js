2842cde02b1fc75b2f8dbe73472ce4ff9ba26f80
David Vera Castillo <indigodavid@gmail.com>
2022-12-08 16:06:12 -0500

    ✅ Clear search on location change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Countries from './pages/Countries';
 import Navbar from './components/Navbar';
+import { searchSlice } from './redux/search/search';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    dispatch(searchSlice.actions.clearCurrent());
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
